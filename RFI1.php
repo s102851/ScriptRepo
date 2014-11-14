8066ec3525a1d538765fdda7ecf4f375339a8874
Roel van Happen <ThisIsAMail>
2014-11-14 11:44:12 +0100

    v3

--- RFI1.php
+++ RFI1.php
@@ -1,3 +1,18 @@
 <?php
-	include($../../../../../home/level1/key.’.txt’);
+	/*function foo(){
+		global $color;
+
+		include '../../../../../home/level1/key.txt';
+
+		echo "A $color $fruit";
+	}*/
+	$f = fopen("../../../../../home/level1/key.txt", "r");
+	
+	// Read line by line until end of file
+	while(!feof($f)) { 
+	    echo fgets($f) . "<br />";
+	}
+	
+	fclose($f);
+	//include($../../../../../home/level1/key.’.txt’);
 	?>
